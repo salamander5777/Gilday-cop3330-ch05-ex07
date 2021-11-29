213fa98f50082d0a08a5e24182bb5263eb9637af
salamander5777 <77403008+salamander5777@users.noreply.github.com>
2021-11-28 21:29:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,17 @@
  */
 
 #include "main.h"
+
+int main() {
+  double a, b, c;
+  std::cout << "Input the value of 'a': ";
+  std::cin >> a;
+  std::cout << "Input the value of 'b': ";
+  std::cin >> b;
+  std::cout << "Input the value of 'c': ";
+  std::cin >> c;
+
+  root_solver(a, b, c);
+
+  //The results appear plausible and are believed to be correct as they appear to be the same results when worked out by hand or when compared to the results of an online quadratic calculator.
+}
